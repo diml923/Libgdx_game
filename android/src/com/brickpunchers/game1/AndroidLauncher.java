d45bda84dea51e467af7ae3955519d977e3c6f6b
Jonas Nikula <jonas.nikula@student.tut.fi>
2018-12-26 18:09:22 +0200

    Initial commit

---
+++ android/src/com/brickpunchers/game1/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.brickpunchers.game1;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.brickpunchers.game1.GdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GdxGame(), config);
+	}
+}
